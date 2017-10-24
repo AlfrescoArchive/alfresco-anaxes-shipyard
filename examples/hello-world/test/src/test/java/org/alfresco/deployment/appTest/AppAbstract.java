0fdca9ed81a7823a255b966e39e0b8409ba38a42
subaprasanna <subashni.prasanna@alfresco.com>
2017-10-24 11:42:54 +0100

    Deploy-132/Deploy-37 - Automated test for hello world app (#12)

--- examples/hello-world/test/src/test/java/org/alfresco/deployment/appTest/AppAbstract.java
+++ examples/hello-world/test/src/test/java/org/alfresco/deployment/appTest/AppAbstract.java
@@ -1,30 +1,29 @@
 package org.alfresco.deployment.appTest;
 
+import java.net.URL;
 import java.util.Properties;
 
 import org.testng.annotations.BeforeSuite;
 
 public class AppAbstract
 {
-    protected static String appServicesUrl;
     protected static String appUrl;
     Properties appProperty = new Properties();
+    protected static URL url ;
 
     /**
      * The before suit will load test properties file and load the same.
      */
-    
     @BeforeSuite 
     public void initialSetup() throws Exception
     {
-            appProperty.load(this.getClass().getClassLoader().getResourceAsStream("test.properties"));
-            appUrl = readProperty("app.url");
-            if(appUrl.isEmpty())
-            {
-            	throw new Exception("please set the app-url details in the properties file");
-            }
-            appServicesUrl = appUrl + "/" + "hello";
-        
+        appProperty.load(this.getClass().getClassLoader().getResourceAsStream("test.properties"));
+        appUrl = readProperty("app.url");
+        if(appUrl.isEmpty() || !(appUrl.contains("hello")))
+        {
+            throw new Exception("please set the app-url details in the properties file");
+        }
+        url = new URL(appUrl);
     }
     
     private String readProperty(String propertyType)
