2992ddbb81066b946ee9263c1732042a69a347c3
Cristina Sirbu <cristina.sirbu@ness.com>
2017-11-01 10:54:40 +0200

    Solved CORS header error

--- examples/hello-world/service/src/main/java/org/alfresco/deployment/sample/HelloController.java
+++ examples/hello-world/service/src/main/java/org/alfresco/deployment/sample/HelloController.java
@@ -2,6 +2,7 @@ package org.alfresco.deployment.sample;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,15 @@ public class HelloController
     @RequestMapping(path = "/{key}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HelloText> getHelloText(@PathVariable String key)
     {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+
         HelloText helloText = helloTextRepository.findOne(key);
         if (helloText == null)
         {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(headers,HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<HelloText>(helloText, HttpStatus.OK);
+        return new ResponseEntity<HelloText>(helloText,headers, HttpStatus.OK);
                 
     }
 
@@ -33,27 +37,33 @@ public class HelloController
             consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<HelloText> createHelloText(@RequestBody HelloText helloText)
     {
-        return new ResponseEntity<HelloText>(helloTextRepository.save(helloText), HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        return new ResponseEntity<HelloText>(helloTextRepository.save(helloText), headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(path = "/{key}", method = RequestMethod.PUT, 
             consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<HelloText> updateHelloText(@RequestBody HelloText helloText)
     {
-        return new ResponseEntity<HelloText>(helloTextRepository.save(helloText), HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        return new ResponseEntity<HelloText>(helloTextRepository.save(helloText), headers, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/{key}", method = RequestMethod.DELETE)
     public ResponseEntity<?> updateHelloText(@PathVariable String key)
     {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
         try
         {
             helloTextRepository.delete(key);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
         }
         catch (Exception e)
         {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); 
+            return new ResponseEntity<>(headers, HttpStatus.INTERNAL_SERVER_ERROR); 
         }
     }
 }
