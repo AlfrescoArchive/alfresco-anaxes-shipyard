c0bffabd7e3b30b008349e8a64aa91b8306fae0d
Jared Ottley <jared.ottley@alfresco.com>
2017-11-01 16:51:46 -0600

    [DEPLOY-136] Implement Sample App UI -Add Global CORS Configuration -Allowed origins can be provided in a comma-seperated list using the cors.allowedOrigins property in application.properties -Add to tests to make the requests use CORS and test for the header Access-Control-Allow-Origin in the response.

--- examples/hello-world/service/src/main/java/org/alfresco/deployment/sample/Application.java
+++ examples/hello-world/service/src/main/java/org/alfresco/deployment/sample/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
+@Import({CorsConfiguration.class})
 public class Application
 {
 	public static void main(String[] args)
