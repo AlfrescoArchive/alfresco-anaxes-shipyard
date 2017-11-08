fbe09a669f18b576539db9c2ebfc42bb468ffd59
Ray Gauss II <rgauss@rgauss.com>
2017-11-08 13:58:10 -0500

    DEPLOY-164: Add Ingress to Hello World
    - Fixed URL scripts for Minikube

--- examples/hello-world/scripts/get-app-url.sh
+++ examples/hello-world/scripts/get-app-url.sh
@@ -17,8 +17,8 @@ CONTEXT=$(kubectl config current-context)
 
 if [[ $CONTEXT == "minikube" ]] ; then
     IP=$(minikube ip)
-    PORT=$(kubectl get service $RELEASE $NAMESPACE -o jsonpath={.spec.ports[0].nodePort})
-    echo "http://$IP:$PORT"
+    PATH=$(kubectl get ingresses $RELEASE-ingress $NAMESPACE -o jsonpath={.spec.rules[0].http.paths[0].path})
+    echo "http://$IP$PATH"
 else
     URL=$(kubectl get services $RELEASE $NAMESPACE -o jsonpath={.status.loadBalancer.ingress[0].hostname})
     echo "http://$URL"
