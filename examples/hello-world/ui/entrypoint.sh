a5793e1994a339dd67b970c09062c2c1490be0ba
Sergiu-Alexandru Vidrascu <p3700456@L3700100608.ness.com>
2017-11-09 12:01:10 +0200

    Change entrypoint sed command

--- examples/hello-world/ui/entrypoint.sh
+++ examples/hello-world/ui/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i s/HELLO_BACKEND_URL/"$HELLO_BACKEND_URL"/g /usr/share/nginx/html/app.config.json
+sed -i s%HELLO_BACKEND_URL%"$HELLO_BACKEND_URL"%g /usr/share/nginx/html/app.config.json
 
 nginx -g "daemon off;"
